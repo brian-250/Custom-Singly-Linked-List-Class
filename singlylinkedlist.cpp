6cedcfdb1b564804660f4e576655186d9d47bac5
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-30 14:05:45 -0800

    Define pushBack() member function

--- singlylinkedlist.cpp
+++ singlylinkedlist.cpp
@@ -4,3 +4,19 @@
 
 template<typename T>
 SinglyLinkedList<T>::SinglyLinkedList() : head_(nullptr), tail_(nullptr), size_(0) {}
+
+template<typename T>
+void SinglyLinkedList<T>::pushBack(T data) {
+   // new_node pointer is initialized to
+   // a dynamically allocated Node object
+   Node<T>* new_node = new Node<T>(data);
+   if ((head_ == nullptr) && (tail_ == nullptr)) {
+       head_ = new_node;
+       tail_ = new_node;
+       size_++;
+   } else {
+       tail_->next_ = new_node;
+       tail_ = tail_->next_;
+       size_++;
+   }
+}
