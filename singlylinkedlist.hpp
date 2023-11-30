2f8f8b19f6c1c9a083db5ec4df9b6a5ddf338052
brian-250 <brianmilian@csu.fullerton.edu>
2023-11-30 12:01:51 -0800

    Declare class SinglyLinkedList

---
+++ singlylinkedlist.hpp
@@ -0,0 +1,21 @@
+#ifndef SINGLYLINKEDLIST_HPP
+#define SINGLYLINKEDLIST_HPP
+
+#include "node.hpp"
+
+// template<typename T> is written again to correctly;
+// templated with type paramter T to ensure Node<T> instances
+// used as member variables are instantiated with the same type
+template<typename T>
+class SinglyLinkedList {
+    public:
+        void pushBack(T data);
+        void popBack();
+
+    private:
+        Node<T>* head_;
+        Node<T>* tail_;
+        int size_;
+};
+
+#endif
